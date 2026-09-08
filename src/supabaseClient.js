9e3f826d179213614bdd6195431d7b930adb07fa
okivictor01-bit <okivictor01@gmail.com>
2026-09-08 16:45:27 +0100

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+// REPLACE THESE WITH YOUR ACTUAL SUPABASE KEYS
+const supabaseUrl = 'PASTE_YOUR_PROJECT_URL_HERE'
+const supabaseAnonKey = 'PASTE_YOUR_ANON_PUBLIC_KEY_HERE'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
